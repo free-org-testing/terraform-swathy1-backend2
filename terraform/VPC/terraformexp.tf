e30148c008a6e252c7ff6bd754c4bf6b1ac42836
Edge Opsx <edgeops@ust-global.com>
2023-12-11 15:21:08 +0000

    TERRAFORM-SWATHY1-00: Initial commit

---
+++ terraform/VPC/terraformexp.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=0.7.0"
+    }
+  }
+}
+
+# Azure AD Group
+# Documentation: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/service_principal
+resource "azuread_group" "changeme_simple_group" {
+  name = "changeme-simple-group-name"
+}
